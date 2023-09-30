367d9389fca97ebcfcc20f61ccd6593b46e3b0bc
Vrushank Humnabadkar <97488105+Draco4520@users.noreply.github.com>
2023-10-01 00:09:28 +0530

    UpdatedBellmanFordAlgorithm.java

--- 4 . Graphs/BellmanFordAlgorithm.java
+++ 4 . Graphs/BellmanFordAlgorithm.java
@@ -64,3 +64,29 @@ public class BellmanFordAlgorithm {
         bellmanFord(edges, V, startNode);
     }
 }
+
+/*
+The provided Java program implements the Bellman-Ford algorithm to find the shortest distances from a given starting node to all other nodes in a directed graph. Here's a description of the program:
+
+BellmanFordAlgorithm Class:
+
+This is the main class that contains the bellmanFord method for performing the Bellman-Ford algorithm and the main method to demonstrate its usage.
+Edge Class:
+
+The Edge class is a nested class used to represent edges in the graph. Each edge has a source node (src), a destination node (dest), and a weight (weight) associated with it.
+bellmanFord Method:
+
+The bellmanFord method takes three parameters: a list of Edge objects representing the graph's edges (edges), the number of vertices (V), and the starting node (start) from which the shortest distances are to be calculated.
+It initializes an array dist to store the shortest distances from the starting node to all other nodes. Initially, all distances are set to Integer.MAX_VALUE except for the starting node, which is set to 0.
+The method then performs the Bellman-Ford algorithm by iterating over the vertices V - 1 times (where V is the number of vertices). In each iteration, it relaxes the edges by checking if there is a shorter path to a vertex through the current vertex. If a shorter path is found, it updates the distance.
+After the main loop, the method checks for the presence of negative-weight cycles by iterating over the edges again. If a shorter path is found, it indicates the existence of a negative-weight cycle in the graph.
+Finally, it prints the shortest distances from the starting node to all other nodes.
+main Method:
+
+The main method is the entry point of the program.
+It creates a graph with 5 vertices and initializes a list of Edge objects (edges) to represent the graph's edges.
+Each Edge object in the edges list represents a directed edge with its source, destination, and weight.
+It specifies the starting node (startNode) from which to find the shortest distances.
+It calls the bellmanFord method with the graph's edges, the number of vertices, and the starting node as arguments to calculate and display the shortest distances.
+Overall, this program demonstrates the Bellman-Ford algorithm for finding the shortest distances in a weighted directed graph. It also checks for the presence of negative-weight cycles, which can affect the correctness of the shortest path calculations.
+    */
