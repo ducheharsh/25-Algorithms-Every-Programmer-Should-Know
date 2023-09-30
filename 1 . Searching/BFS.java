5637476d4be069de5a60504e65759a6f5162775c
Nilesh Telang <95858066+NileshTelang@users.noreply.github.com>
2023-09-30 23:53:41 +0530

    Update BFS.java

--- 1 . Searching/BFS.java
+++ 1 . Searching/BFS.java
@@ -58,7 +58,36 @@ public class BFS {
         g.addEdge(2, 5);
         g.addEdge(2, 6);
 
+
+
+        
+
         System.out.println("Breadth-First Traversal (starting from vertex 0): ");
         g.BFS(0);
     }
 }
+
+
+
+/*This Java program represents a graph data structure and performs a Breadth-First Search (BFS) traversal on the graph, starting from a specified vertex. Here's a short description of the key components and functionality:
+
+1. `Graph` class:
+   - The `Graph` class represents a graph with a specified number of vertices (`V`).
+   - It uses an adjacency list to store the edges between vertices.
+   - The constructor initializes the graph by creating an adjacency list for each vertex.
+   - The `addEdge` method adds an edge between two vertices by updating the adjacency list.
+
+2. `BFS` method:
+   - This method performs a Breadth-First Search traversal starting from a given vertex.
+   - It uses a boolean array `visited` to keep track of visited vertices.
+   - It utilizes a queue (`LinkedList`) to implement the BFS algorithm.
+   - The algorithm starts with the specified vertex, marks it as visited, and enqueues it.
+   - It then iterates through the queue, dequeuing vertices, printing them, and enqueuing their unvisited neighbors until the queue is empty.
+   - This ensures that vertices are visited in breadth-first order, i.e., all vertices at the current level are visited before moving to the next level.
+
+3. `main` method:
+   - In the `main` method, an instance of the `Graph` class is created with 7 vertices.
+   - Edges are added to the graph to form a specific structure.
+   - The BFS traversal is initiated from vertex 0, and the result is printed to the console.
+
+In this example, the BFS traversal starting from vertex 0 would print the vertices in the following order: 0, 1, 2, 3, 4, 5, 6. This program demonstrates the fundamental BFS algorithm for exploring and traversing graphs in a breadth-first manner.*/
