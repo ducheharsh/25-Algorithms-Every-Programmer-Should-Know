181b98f86e5b38aa03479257a94f72602c7e61f1
shraddha krishnagoudar <146472639+09shraddhagoudar@users.noreply.github.com>
2023-10-01 00:22:37 +0530

    Update TopologicalSort.java

--- 4 . Graphs/TopologicalSort.java
+++ 4 . Graphs/TopologicalSort.java
@@ -58,4 +58,33 @@ public class TopologicalSort {
 
         g.topologicalSort();
     }
-}
+}/*This Java program performs a topological sort on a directed acyclic graph (DAG) using depth-first search (DFS) and prints the topological ordering. Here's a concise description of its key components and functionality:
+
+Graph class:
+
+Represents a directed graph with a specified number of vertices (V).
+Utilizes an adjacency list to store the graph's edges.
+The addEdge method adds a directed edge from vertex v to vertex w.
+topologicalSortUtil method:
+
+A recursive utility function for performing topological sort.
+Marks the current vertex as visited and explores its adjacent vertices.
+Pushes vertices onto a stack in a specific order based on the topological sorting.
+topologicalSort method:
+
+Initiates the topological sort process for the entire graph.
+Utilizes a stack to store the topological ordering.
+Iterates through each vertex and calls topologicalSortUtil for unvisited vertices to perform the DFS-based topological sort.
+Prints the topological ordering after completing the traversal.
+main method:
+
+Creates an instance of the Graph class with 6 vertices.
+Adds directed edges to create a directed acyclic graph (DAG).
+Calls the topologicalSort method to find and print the topological sorting order of the vertices.
+This program is designed to find a valid topological ordering for a directed acyclic graph, which is a useful operation in various applications, such as scheduling and dependency resolution, where tasks or elements must be processed in a specific order without cycles.*/
+
+
+
+
+
+
